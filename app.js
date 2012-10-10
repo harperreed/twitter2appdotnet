513dc6d6484e13c0af9805f7381f39719b6f006b
Harper Reed <harper@nata2.org>
2012-10-10 13:34:25 -0500

    initial import of sync

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var twitter = require('ntwitter');
 var AppDotNet = require('appdotnet');
 
 config = {
-  'appdotnet_token': "AQAAAAAAAVmSzOtow76LyYUf5sePLRe92d-GpOq0eZGzpJH80yJMi9mgtg5SQR2NGmSP_xt0Be1ecK2XoeOzkNwL4qTLUIdnVA",
-  'twitter_user_id': '1497',
+  'appdotnet_token': "AQAAAAAAAVoUOk7ehUvMUr9wzJvUHuyf_aq0Alw49srv7UNw3u8Hfg-Oxmx1HGVwx3wao0gb6Axl_jiaz-MhpKnBgkU94aZsknjCPVdngVVb8KWZjQAFoGIo6P94593QnzTODY1GNE7s"
+  'twitter_user_id': '813286',
   'twitter_consumer_key': 'O4S7hbxaPkovNiyU6EQ',
   'twitter_consumer_secret': 'RjthdRKq2KcCGpq3bpHqe3W8Ps2fyjaFhkkVs10OZOQ',
   'twitter_access_token_key': '813286-TfvGT9oyipkVQtG5ShsU9oy1FleSGADIDfuDyrEcyXo',
@@ -29,8 +29,6 @@ twit.stream('statuses/filter', {'follow':config.twitter_user_id}, function(strea
     console.log('Received tweet id: '+params['id'])
     appdotnet_client.createPost(params, function (err, post) {
         console.log('Posted tweet id: '+params['id']+' to app.net')
-        console.log(post);
-        console.log(err);
     });  
   });
 });
