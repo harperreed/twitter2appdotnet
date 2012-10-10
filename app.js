dbda5523de20fe0b81f7891b641cfd79caf4658f
Harper Reed <harper@nata2.org>
2012-10-10 14:05:09 -0500

    updated to better check for config

--- app.js
+++ app.js
@@ -10,7 +10,34 @@ config = {
   'twitter_access_token_secret': process.env.TWITTER_TOKEN_SECRET
 }
 
-if (config.appdotnet_token){
+
+if (typeof config.appdotnet_token== "undefined"){
+  console.log('No appdotnet token.');
+  process.exit(1);
+}
+
+if (typeof config.twitter_user_id== "undefined"){
+  console.log('No twitter user_id.');
+  process.exit(1);
+}
+
+if (typeof config.twitter_consumer_key== "undefined"){
+  console.log('No twitter_consumer_key.');
+  process.exit(1);
+}
+
+if (typeof config.twitter_consumer_secret== "undefined"){
+  console.log('No twitter_consumer_secret.');
+  process.exit(1);
+}
+
+if (typeof config.twitter_access_token_key== "undefined"){
+  console.log('No twitter_access_token_key.');
+  process.exit(1);
+}
+
+if (typeof config.twitter_access_token_secret== "undefined"){
+  console.log('No twitter_access_token_secret.');
   process.exit(1);
 }
 
