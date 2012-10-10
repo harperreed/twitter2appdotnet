b56bdde8961b8f2376ca4b90e71012c8d81e98ba
Harper Reed <harper@nata2.org>
2012-10-10 13:09:50 -0500

    removed config file

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
 var twitter = require('ntwitter');
 var AppDotNet = require('appdotnet');
 
-var config = require('./config')
+config = {
+  'appdotnet_token': "AQAAAAAAAVmSzOtow76LyYUf5sePLRe92d-GpOq0eZGzpJH80yJMi9mgtg5SQR2NGmSP_xt0Be1ecK2XoeOzkNwL4qTLUIdnVA",
+  'twitter_user_id': '1497',
+  'twitter_consumer_key': 'O4S7hbxaPkovNiyU6EQ',
+  'twitter_consumer_secret': 'RjthdRKq2KcCGpq3bpHqe3W8Ps2fyjaFhkkVs10OZOQ',
+  'twitter_access_token_key': '813286-TfvGT9oyipkVQtG5ShsU9oy1FleSGADIDfuDyrEcyXo',
+  'twitter_access_token_secret': '5HwTqyuSxP7ddvYTSy8U0cyfQu5sxRpSXx9mLz8pzUs'
+}
 
 appdotnet_client = new AppDotNet(config.token)
 
